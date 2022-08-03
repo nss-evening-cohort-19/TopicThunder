08f745c6ac73e726414b985fc46726c972595fdf
Patrick Burns <burns212@gmail.com>
2022-08-02 19:07:52 -0500

    tried some API calls based on my data design, changed sample data a lot

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // import { Button } from 'react-bootstrap';
 // import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import { addFollowRelation } from '../api/usersData';
 
 function Home() {
   const { user } = useAuth();
@@ -17,9 +18,9 @@ function Home() {
     >
       <h1>Hello {user.displayName}! </h1>
       <p>Click the button below to logout!</p>
-      {/* <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button> */}
+      <button variant="danger" type="button" size="lg" className="copy-btn" onClick={() => addFollowRelation('-N8V1NJy9iNJDisnYJno', '-N8V12atHLWuVd9WQu1K')}>
+        Add relationship
+      </button>
     </div>
   );
 }
