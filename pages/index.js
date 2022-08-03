53ab67a2607687989ebf3257169a84de821d3b69
Michael Pennington <mwpennington@live.com>
2022-08-02 20:48:30 -0500

    setting everything up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 // import { Button } from 'react-bootstrap';
-// import { signOut } from '../utils/auth';
+import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
@@ -17,9 +17,9 @@ function Home() {
     >
       <h1>Hello {user.displayName}! </h1>
       <p>Click the button below to logout!</p>
-      {/* <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
+      <button type="button" size="lg" className="btn-primary" onClick={signOut}>
         Sign Out
-      </Button> */}
+      </button>
     </div>
   );
 }
