805a70a441dfb9bce992835833f58309895a251a
Jeremiah King <usaxray25@gmail.com>
2022-08-04 19:58:31 -0500

    added and rendered pin card

--- pages/today.js
+++ pages/today.js
@@ -1,15 +1,20 @@
-import React from 'react';
-// import PinCard from '../components/PinCard';
+import React, { useState, useEffect } from 'react';
+import { getPinByFirebaseKey } from '../api/pinsData';
+import PinCard from '../components/PinCard';
 // import PropTypes from 'prop-types';
 
 function Today() {
+  const [pin, setPin] = useState([]);
+
+  useEffect(() => {
+    getPinByFirebaseKey('-N8V45RB3KJpf4kMqBnw').then(setPin);
+  }, []);
   return (
     <>
       <div>Today</div>
+      <PinCard pinObj={pin} />
     </>
   );
 }
-
-Today.propTypes = {};
 
 export default Today;
