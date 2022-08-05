5fe0c0291e597bb19cc15935ec013c285b770c65
Patrick Burns <burns212@gmail.com>
2022-08-04 21:04:50 -0500

    added collections data and deleted obsolete API calls

--- api/pinsData.js
+++ api/pinsData.js
@@ -21,18 +21,8 @@ const getPinsByUser = (userFirebaseKey) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-const getPinsByBoard = (boardFirebaseKey) => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/boards/${boardFirebaseKey}.json`)
-    .then((response) => {
-      const gatherPinsPromises = response.data.pins.map((pinFirebaseKey) => getPinByFirebaseKey(pinFirebaseKey));
-      Promise.all(gatherPinsPromises).then((values) => resolve(Object.values(values)));
-    })
-    .catch((error) => reject(error));
-});
-
 export {
   getAllPins,
   getPinByFirebaseKey,
   getPinsByUser,
-  getPinsByBoard,
 };
