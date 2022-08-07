8187f189b0952e58644e44fdc934c6d05696b920
Patrick Burns <burns212@gmail.com>
2022-08-07 14:37:24 -0500

    fixing delete calls

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,8 +1,24 @@
-import { deleteBoardShallow, getBoardsByUser } from './boardsData';
+import axios from 'axios';
+import { deleteBoardShallow } from './boardsData';
 import { getBoardsThatContainGivenPin, getPinsContainedByGivenBoard, removePinFromBoard } from './collectionsData';
 import { getWhoFollowsUser, getWhoUserFollows, removeFollow } from './followsData';
-import { deletePinShallow, getPinsByUser } from './pinsData';
+import { deletePinShallow } from './pinsData';
 import { deleteUserShallow } from './usersData';
+import { clientCredentials } from '../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+const mergedGetBoardsByUser = (userHandle) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/boards.json?orderBy="user"&equalTo="${userHandle}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+const mergedGetPinsByUser = (userHandle) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/pins.json?orderBy="user"&equalTo="${userHandle}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
 
 const deletePin = (pinFirebaseKey) => new Promise((resolve, reject) => {
   getBoardsThatContainGivenPin(pinFirebaseKey).then((response) => {
@@ -29,10 +45,10 @@ const deleteUser = (userHandle) => new Promise((resolve, reject) => {
       getWhoUserFollows(userHandle).then((arrayOfFollows) => {
         const deleteOutgoingFollows = arrayOfFollows.map((outgoingFollowUser) => removeFollow(userHandle, outgoingFollowUser.handle));
         Promise.all(deleteOutgoingFollows).then(() => {
-          getPinsByUser(userHandle).then((arrayOfPinObjects) => {
+          mergedGetPinsByUser(userHandle).then((arrayOfPinObjects) => {
             const deleteUsersPins = arrayOfPinObjects.map((userPin) => deletePin(userPin.firebaseKey));
             Promise.all(deleteUsersPins).then(() => {
-              getBoardsByUser(userHandle).then((arrayOfBaordObjects) => {
+              mergedGetBoardsByUser(userHandle).then((arrayOfBaordObjects) => {
                 const deleteUsersBoards = arrayOfBaordObjects.map((userBoard) => deleteBoard(userBoard.firebaseKey));
                 Promise.all(deleteUsersBoards).then(() => {
                   deleteUserShallow(userHandle).then(resolve);
